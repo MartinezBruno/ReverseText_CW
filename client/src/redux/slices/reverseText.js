f18b491cf4c613dcc95edd581d1b90e1dff69ae7
Bruno Martinez <brunomar006@gmail.com>
2022-07-28 18:57:14 -0300

    create slice reverseText

---
+++ client/src/redux/slices/reverseText.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const reverseTextSlice = createSlice({
+  name: 'reverseText',
+  initialState: {
+    reversedStrings: [],
+    warning: false,
+  },
+  reducers: {
+    AddReversedString: (state, action) => {
+      state.reversedStrings.unshift(action.payload)
+    },
+    ClearReversedStrings: state => {
+      state.reversedStrings = []
+    },
+    SetWarning: (state, action) => {
+      state.warning = action.payload
+    },
+  },
+})
+
+export default reverseTextSlice.reducer
+
+export const { AddReversedString, ClearReversedStrings, SetWarning } = reverseTextSlice.actions
+
+export const getReversedString = payload => async dispatch => {
+  try {
+    const reversedText = await axios.get(`http://localhost:3001/reverseText?text=${payload}`)
+    console.log(reversedText.data)
+    dispatch(AddReversedString(reversedText.data))
+  } catch (error) {
+    console.log(error.response.data)
+    if (error.response.data.error === 'no text') {
+      dispatch(SetWarning(true))
+    }
+  }
+}
+
+export const clearReversedStrings = () => dispatch => {
+  dispatch(ClearReversedStrings())
+}
+
+export const closeWarning = () => dispatch => {
+  dispatch(SetWarning(false))
+}
